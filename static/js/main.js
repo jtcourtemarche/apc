ef88268f2ccf42788ed40d64058829f164291262
jtcourtemarche <jacob.courtemarche@gmail.com>
2018-08-14 23:06:04 -0400

    Disable textarea while crawler is running

--- static/js/main.js
+++ static/js/main.js
@@ -4,14 +4,17 @@ var $form = $('#apc-form');
 
 $form.submit(function() {
 	socket.emit('run_crawler', {data:$form.serializeArray()})
+	$('textarea').prop('disabled', true);
 	return false;
 });
 
 socket.on('payload', function(data) {
 	if (data.includes('[Complete]')) {
 		$('#output-log').append('<div class="alert alert-success" role="alert">'+ data +'</div><hr>');
+		$('textarea').prop('disabled', false);
 	} else if (data.includes('[Error]')) {
 		$('#output-log').append('<div class="alert alert-danger" role="alert">'+ data +'</div><hr>');	
+		$('textarea').prop('disabled', false);
 	}
 	else {
 		$('#output-log').append('<span>'+data+'</span><br/>');
