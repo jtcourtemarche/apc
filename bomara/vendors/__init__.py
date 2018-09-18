42d6b530924d203c660723cc34227475145830b9
jtcourtemarche <jacob.courtemarche@gmail.com>
2018-09-18 15:17:38 -0400

    Restrict import to only .py files

--- bomara/vendors/__init__.py
+++ bomara/vendors/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 import os
 
-__all__ = [module.replace('.py', '') for module in os.listdir(os.getcwd()+'/bomara/vendors') if module[0] != '_']
+__all__ = [module for module in os.listdir(os.getcwd()+'/bomara/vendors') if module[0] != '_' and module[-3:] == '.py']
+__all__ = [module.replace('.py', '') for module in __all__]
